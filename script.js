30f400051d9aebcc9396ca04e3f93376d63c1382
Wardun-00 <wardun.bd@gmial.com>
2025-09-10 01:43:49 +0600

    added copy and call section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// heart icon react 
 const heartCount = document.getElementById('heartCount');
 const cardHeart = document.querySelectorAll('.card-heart');
 
@@ -7,14 +9,65 @@ for (const heart of cardHeart) {
   heart.addEventListener('click', function () {
     if (heart.classList.contains('active')) {
       heart.classList.remove('active');
-      heart.src = './assets/2heart.png'; // unliked
+      heart.src = './assets/2heart.png'; 
       count--;
     } else {
       heart.classList.add('active');
-      heart.src = './assets/heart.png'; // liked
+      heart.src = './assets/heart.png'; 
       count++;
     }
 
     heartCount.textContent = count;
   });
 }
+
+
+// call button credit cut
+
+const creditCount = document.getElementById('creditCount');
+
+// call button all 
+const callButton = document.querySelectorAll('.fa-phone');
+
+let credit = 100;
+for(const callBtn of callButton){
+    callBtn.parentElement.addEventListener('click', function(){
+        alert("📞 Calling Now... 📞");
+
+        // 10ta credit kete nibe
+
+        if(credit >= 10){
+            credit -= 10;
+            creditCount.textContent =credit;
+
+        }
+
+        else{
+            alert('❌ Opps Sorry, Please Recharge credit ❌')
+        }
+    })
+}
+
+
+// copy button er kaz korar jonno
+const copyButtons = document.querySelectorAll('.fa-copy');
+
+copyButtons.forEach(copyBtn => {
+  copyBtn.closest('button').addEventListener('click', function() {
+    // card div work
+    const card = copyBtn.closest('.rounded-2xl');
+    if (!card) return;
+
+    // number span er kaz
+    const numberSpan = card.querySelector('span.font-extrabold');
+    if (!numberSpan) return;
+
+    const number = numberSpan.textContent.trim();
+
+    navigator.clipboard.writeText(number)
+      .then(() => alert(`✅ Number ${number} copied to clipboard!`))
+      .catch(() => alert('❌ Copy failed!'));
+  });
+});
+
+
